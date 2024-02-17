58f44061b438c09b7d88d8ae2959170de08bc11e
DevSadit <de.sadit@gmail.com>
2024-02-17 18:58:27 +0600

    file created and triangle finished

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function calculateTriangle() {
+  const b = document.getElementById(`base`);
+  const x = parseFloat(b.value);
+  const h = document.getElementById(`height`);
+  const y = parseFloat(h.value);
+  const area = 0.5 * x * y;
+
+//   way 01
+  const spanp = document.getElementById(`para-area`);
+  spanp.innerText = `The Area of the Triangle is ${area}`;
+//   way 02
+  // const areaBox = document.getElementById(`area-box`);
+  // const p = document.createElement(`p`);
+  // p.textContent = `the area of the triangle is ${area}`
+  // areaBox.appendChild(p);
+}
+
+
+
+function calculateRectangle() {
+  const w = document.getElementById(`width`);
+  const x = parseFloat(width.value);
+  const l = document.getElementById(`length`);
+  const y = parseFloat(length.value);
+  const area = 0.5 * x * y;
+
+  //   way 01
+  const spanp = document.getElementById(`para-area`);
+  spanp.innerText = `The Area of the Rectangle is ${area}`;
+  //   way 02
+  // const areaBox = document.getElementById(`area-box`);
+  // const p = document.createElement(`p`);
+  // p.textContent = `the area of the triangle is ${area}`
+  // areaBox.appendChild(p);
+}
